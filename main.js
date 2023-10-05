fa8cb413442be371fa794c35a87cfa7b610e193e
Lucas Rordigues Garcia <103426035+lucasrg20@users.noreply.github.com>
2023-10-05 09:33:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-document.querySelectorAll('tecla')
-
 function tocaSomPom (){
     document.querySelector('#som_tecla_pom').play();
 }
+document.querySelectorAll('.tecla')
+
+const listadeteclas = document.querySelectorAll(.'tecla')
+listadetecla[1].onclick = tocaSomPomClap;
+listadetecla[2].onclick = tocaSomTim;
+listadetecla[3].onclick = tocaSomPuff;
